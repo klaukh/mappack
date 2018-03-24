b8e9bb76ac42cc48cc1142f4cdea7f6a371d61c7
klauk <laukhuf@gmail.com>
2018-03-24 16:57:16 -0400

    addMappackTiles function; test successful

---
+++ R/tiles.R
@@ -0,0 +1,41 @@
+
+#' Add local tiles to a leaflet map
+#'
+#' Adds local tiles provided by the \emph{mappack} package to a \code{leaflet} map.
+#' '
+#' @param map a map widget object created from \code{\link{leaflet}()}
+#' '
+#' @param layerId the layer id
+#' @param group the name of the grou the newly created layers should belong to
+#' @param options a list of extra options for tile layers, popups, paths
+#'   (circles, rectangles, polygons, ...), or other map elements
+#' @return the new \code{map} object
+#' @seealso \code{\link{tileOptions}}, \code{\link{WMSTileOptions}},
+#'   \code{\link{popupOptions}}, \code{\link{markerOptions}},
+#'   \code{\link{pathOptions}}
+#' @references The Leaflet API documentation:
+#'   \url{http://leafletjs.com/reference.html}
+#' @import leaflet shiny
+#'
+#' @export
+addMappackTiles <- function(map, layerId = NULL, group = NULL,
+                            options = leaflet::tileOptions()) {
+  shiny::addResourcePath('mappack_tiles', system.file('map', package = 'mappack'))
+
+  # test for a leaflet map
+  if (is.null(map)) stop("addMappackTiles: no map provided... exiting")
+
+  # directly set tileOptions for the provided tiles
+  options$minZoom <- 0
+  options$maxZoom <- 8
+  options$tms <- TRUE
+
+  map <- leaflet::addTiles(map
+    , urlTemplate = "mappack_tiles/{z}/{x}/{y}.png"
+    , attribution = "MapPack package, leveraging OpenStreetTiles, Natural Earth, and OpenStreetMap"
+    , layerId = layerId
+    , group = group
+    , options = options)
+
+  return(map)
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mappack
+## mappack
 An R package to provide local tiles for leaflet maps
 
 `mappack` provides an example on how to serve local tiles for `leaflet` maps as part of a package. This package will not be provided on CRAN, nor do I believe it would be accepted on CRAN, due to the large number of files contained inside the package itself. 
@@ -6,3 +6,14 @@ An R package to provide local tiles for leaflet maps
 However, if compiled and served via a local `R` package repository, then the concept provides a powerful way to have a commonly accessible set of local tiles to provide to the local user base.
 
 The tiles themselves are housed in the `inst/` folder, and requires an active `shiny` session to serve them... either through a `shiny` app or gadget (e.g., `mapview` or `mapedit` packages).
+
+```r
+library(leaflet)
+library(mappack)
+
+# run only in a Shiny app or gadget
+leaflet() %>%
+  addMappackTiles(layerId = "mappack", group = "mygroup")
+
+```
+
